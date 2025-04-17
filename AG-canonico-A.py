b7226e23efe78e9c79c5ba7610bf787e4dfb9810
Tincho2910 <tinchogarcia2015@gmail.com>
2025-04-16 22:31:44 -0300

    Update AG-canonico-A.py
    agregue decimal, funcion  y fitnes

--- AG-canonico-A.py
+++ AG-canonico-A.py
@@ -27,4 +27,44 @@ def generarPoblacion(numeroIndividuos, tamano):#Tamaño de la cadena binaria
     return poblacion
 
 
+# pasaje de un binario a decimal
+def decimal(binario):
+    decimal=0
+    puntero=0
+    for i in range (0,29):
+        decimal = decimal + binario[puntero]*(2**(29-puntero))
+        puntero+=1
+        if puntero==29:
+            decimal+=1
+    return decimal
+
+
+
+# Funcion fitness
+def fitnes(poblacion, numeroIndividuos):
+    acum = 0
+    fitness = []
+    
+    indice = 0
+    while indice < numeroIndividuos:
+        x = decimal(poblacion[indice])
+        acum += funcionObjetivo(x)
+        indice += 1
+
+    indice = 0
+    while indice < numeroIndividuos:
+        x = decimal(poblacion[indice])
+        fit = funcionObjetivo(x) / acum 
+        fitness.append(fit)
+        indice += 1
+
+    return fitness
+
+
 poblacion=generarPoblacion(numeroIndividuos=10,tamano=30)
+acum=0
+fit=fitnes(poblacion,10)
+print("               arreglo binario                                                              decimal       funcion           fitnes")
+for i in range (0,9):
+    deci=decimal(poblacion[i])
+    print (poblacion[i], deci ,funcionObjetivo(deci),fit[i])
